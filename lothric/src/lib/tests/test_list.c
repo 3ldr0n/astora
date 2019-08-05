c815872975a451527dc41a62cad27c1f2ee872c4
luiz tagliaferro <luiztagli@hotmail.com>
2019-08-04 21:51:55 -0300

    resolve  a issue #8

--- lothric/src/lib/tests/test_list.c
+++ lothric/src/lib/tests/test_list.c
@@ -3,40 +3,40 @@
 #include "lib/list.h"
 #include "lib/test.h"
 
-int test_instanciate_list(struct list_t *list)
+int test_list_instanciate(struct list_t *list)
 {
-    instanciate_list(list);
+    list_instanciate(list);
     _assert(list->size == 0);
     return 1;
 }
 
-int test_is_empty_list(struct list_t *list)
+int test_list_is_empty(struct list_t *list)
 {
-    _assert(is_list_empty(list));
+    _assert(list_is_empty(list));
     return 1;
 }
 
-int test_append(struct list_t *list)
+int test_list_append(struct list_t *list)
 {
-    append(list, (void *)3);
-    append(list, (void *)1);
-    append(list, (void *)2);
+    list_append(list, (void *)3);
+    list_append(list, (void *)1);
+    list_append(list, (void *)2);
     _assert(list->size == 3);
     _assert(list->head->value == (void *)3);
     _assert(list->tail->value == (void *)2);
     return 1;
 }
 
-int test_remove_first(struct list_t *list)
+int test_list_remove_first(struct list_t *list)
 {
-    remove_first(list);
+    list_remove_first(list);
     _assert(list->size == 2);
     return 1;
 }
 
-int test_remove_last(struct list_t *list)
+int test_list_remove_last(struct list_t *list)
 {
-    remove_last(list);
+    list_remove_last(list);
 
     _assert(list->tail->value == list->head->value);
     _assert(list->size == 1);
@@ -47,15 +47,15 @@ int main()
 {
     struct list_t list;
 
-    test_instanciate_list(&list);
+    test_list_instanciate(&list);
 
-    test_is_empty_list(&list);
+    test_list_is_empty(&list);
 
-    test_append(&list);
+    test_list_append(&list);
 
-    test_remove_first(&list);
+    test_list_remove_first(&list);
 
-    test_remove_last(&list);
+    test_list_remove_last(&list);
 
     printf("%d/%d tests passed\n", tests_passed, tests_run);
 
