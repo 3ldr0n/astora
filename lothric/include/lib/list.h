7fac56c08b428b1893549805fbc59892a2085889
luiz tagliaferro <luiztagli@hotmail.com>
2019-08-04 22:33:02 -0300

    remove as redundancias

--- lothric/include/lib/list.h
+++ lothric/include/lib/list.h
@@ -10,40 +10,30 @@
 
 struct node {
 
-    // ponteiro generico
     const void *value;
 
-    // ponteiro para o proximo nó na lista
     struct node *next;
 
-    // ponteiro para o nó anterior na lista
     struct node *prev;
 };
 
 struct list_t {
 
-    // ponteiro para  cabeça
     struct node *head;
 
-    // ponteiro para o rabo
     struct node *tail;
 
     size_t size;
 };
 
-// inicializa a lista
 void list_instanciate(struct list_t *list);
 
-// verifica se é uma lista vazia
 bool list_is_empty(const struct list_t *list);
 
-// adiciona um elemento na lista
 void list_append(struct list_t *list, const void *value);
 
-// remove o primeiro elemento da lista
 void list_remove_first(struct list_t *list);
 
-// remove o ultimo elemento da lista
 void list_remove_last(struct list_t *list);
 
 #endif
